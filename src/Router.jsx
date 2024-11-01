3105eeb20cb0e05b7b2dc03a3c501d8109d7a371
Kalimah <69271526+hamilak@users.noreply.github.com>
2024-11-01 15:02:14 +0100

    update route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Projects from './pages/projects'
 import Contact from './pages/contact'
@@ -7,15 +7,16 @@ import About from './pages/about'
 import ProjectInformation from './pages/projectInformation'
 
 const Router = () => {
-  return (
+    return (
         <Routes>
-            <Route path='/home' element={<Home />}></Route>
+            <Route path='/' element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<Home />} />
             <Route path='/about' element={<About />}></Route>
             <Route path='/projects' element={<Projects />}></Route>
             <Route path='/contact' element={<Contact />}></Route>
             <Route path='/project/:title' element={<ProjectInformation />}></Route>
         </Routes>
-  )
+    )
 }
 
 export default Router
